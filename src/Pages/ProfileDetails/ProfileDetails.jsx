de62b1951995a119fc0584c8c9ced98cafeccf8f
arifraj199 <karif9514@gmail.com>
2024-06-02 01:01:23 +0600

    similar profile details added

--- src/Pages/ProfileDetails/ProfileDetails.jsx
+++ src/Pages/ProfileDetails/ProfileDetails.jsx
@@ -1,6 +1,7 @@
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import { Button } from "flowbite-react";
+import { useState } from "react";
 import { FaUser } from "react-icons/fa";
 import { IoLocationOutline } from "react-icons/io5";
 import {
@@ -8,19 +9,32 @@ import {
   PiGenderIntersexLight,
 } from "react-icons/pi";
 import { useParams } from "react-router-dom";
+import RelatedProfileCard from "./RelatedProfileCard";
 
 const ProfileDetails = () => {
   const { id } = useParams();
+  const [gender, setGender] = useState(null);
   const { data: member = [], isPending } = useQuery({
-    queryKey: ["member"],
+    queryKey: ["member", id],
     queryFn: async () => {
       const res = await axios.get(`http://localhost:5000/members/${id}`);
-
+      setGender(res.data[0].biodata_type);
       return res.data;
     },
   });
 
-  if (isPending) {
+  const { data: similarMember = [], isPending: pendingSimilarMember } =
+    useQuery({
+      queryKey: ["similarMember", gender],
+      queryFn: async () => {
+        const res = await axios.get(
+          `http://localhost:5000/similarMembers?gender=${gender}`
+        );
+        return res.data;
+      },
+    });
+
+  if (isPending || pendingSimilarMember) {
     return <p>Loading...</p>;
   }
 
@@ -33,45 +47,58 @@ const ProfileDetails = () => {
     occupation,
   } = member[0];
 
-  //   console.log(member);
+  console.log(similarMember);
   return (
-    <div className="flex gap-6 max-w-screen-xl mx-auto my-20">
-      <div className="w-1/2">
-        <img src={profile_image} alt="" />
-      </div>
-      <div className="w-1/2 ">
-        <h2 className="text-3xl">Bio ID: {biodata_id}</h2>
-        <div className="flex gap-6 mt-6">
-          <div className="flex flex-col shadow p-3 items-center gap-2 ">
-            <FaUser className="text-xl"></FaUser>
-            Age: {age}
-          </div>
-          <div className="flex flex-col shadow p-3 items-center gap-2 ">
-            <PiGenderIntersexLight className="text-xl" />
-            Gender: {biodata_type}
-          </div>
-          <div className="flex flex-col shadow p-3 items-center gap-2 ">
-            <IoLocationOutline className="text-xl" />
-            Location: {permanent_division_name}
-          </div>
+    <div className=" max-w-screen-xl mx-auto my-20">
+      <div className="flex gap-6">
+        <div className="w-1/2">
+          <img src={profile_image} alt="" />
+        </div>
+        <div className="w-1/2 ">
+          <h2 className="text-3xl">Bio ID: {biodata_id}</h2>
+          <div className="flex gap-6 mt-6">
+            <div className="flex flex-col shadow p-3 items-center gap-2 ">
+              <FaUser className="text-xl"></FaUser>
+              Age: {age}
+            </div>
+            <div className="flex flex-col shadow p-3 items-center gap-2 ">
+              <PiGenderIntersexLight className="text-xl" />
+              Gender: {biodata_type}
+            </div>
+            <div className="flex flex-col shadow p-3 items-center gap-2 ">
+              <IoLocationOutline className="text-xl" />
+              Location: {permanent_division_name}
+            </div>
 
-          <div className="flex flex-col shadow p-3 items-center gap-2 ">
-            <PiBuildingApartmentLight className="text-xl" />
-            Occupation: {occupation}
+            <div className="flex flex-col shadow p-3 items-center gap-2 ">
+              <PiBuildingApartmentLight className="text-xl" />
+              Occupation: {occupation}
+            </div>
+          </div>
+          <hr className="my-8" />
+          <p className="">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores
+            dolore nihil rerum. Explicabo, laudantium modi obcaecati nisi
+            repudiandae perferendis in saepe maiores. Nulla id quis magnam earum
+            ipsam saepe pariatur, tempore numquam nesciunt dignissimos
+            consequatur voluptatibus excepturi nisi nam quaerat exercitationem
+            sint ut voluptatum fugiat quibusdam! Iure est deserunt adipisci!
+          </p>
+          <div className="flex gap-6 mt-10">
+            <Button>Add To Favorite</Button>
+            <Button>Request Contact Information</Button>
           </div>
         </div>
-        <hr className="my-8" />
-        <p className="">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores
-          dolore nihil rerum. Explicabo, laudantium modi obcaecati nisi
-          repudiandae perferendis in saepe maiores. Nulla id quis magnam earum
-          ipsam saepe pariatur, tempore numquam nesciunt dignissimos consequatur
-          voluptatibus excepturi nisi nam quaerat exercitationem sint ut
-          voluptatum fugiat quibusdam! Iure est deserunt adipisci!
-        </p>
-        <div className="flex gap-6 mt-10">
-          <Button>Add To Favorite</Button>
-          <Button>Request Contact Information</Button>
+      </div>
+      <div className="my-32">
+        <h2 className="text-3xl mb-8">Related Profile</h2>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+          {similarMember.slice(0, 3)?.map((member) => (
+            <RelatedProfileCard
+              key={member.biodata_id}
+              member={member}
+            ></RelatedProfileCard>
+          ))}
         </div>
       </div>
     </div>
---
+++ src/Pages/ProfileDetails/RelatedProfileCard.jsx
@@ -0,0 +1,57 @@
+import { Avatar, Card } from "flowbite-react";
+import { Link } from "react-router-dom";
+
+const RelatedProfileCard = ({ member }) => {
+  const {
+    biodata_id,
+    biodata_type,
+    profile_image,
+    permanent_division_name,
+    age,
+    occupation,
+  } = member;
+  return (
+    <Card className="max-w-sm ">
+      <div className="flex justify-end px-4 pt-4"></div>
+      <div className="flex flex-col items-start pb-10">
+        <div className="flex gap-6 mb-4">
+          <Avatar
+            className="mb-2"
+            img={profile_image}
+            size="lg"
+            status="online"
+          />
+          <div>
+            <p>ID: {biodata_id}</p>
+            <h5 className="my-2 text-xl font-medium text-gray-900 dark:text-white">
+              Gender: {biodata_type}
+            </h5>
+          </div>
+        </div>
+
+        <div className="flex border-t pt-4 gap-4">
+          <span className="text-sm text-gray-500 dark:text-gray-400">
+            Age: {age}
+          </span>
+          <span className="text-sm text-gray-500 dark:text-gray-400">
+            Occupation: {occupation}
+          </span>
+          <span className="text-sm text-gray-500 dark:text-gray-400">
+            Division: {permanent_division_name}
+          </span>
+        </div>
+
+        <div className="mt-4 flex space-x-3 lg:mt-6">
+          <Link
+            to={`/profileDetails/${biodata_id}`}
+            className="inline-flex items-center rounded-lg bg-cyan-700 px-4 py-2 text-center text-sm font-medium text-white hover:bg-cyan-800 focus:outline-none focus:ring-4 focus:ring-cyan-300 dark:bg-cyan-600 dark:hover:bg-cyan-700 dark:focus:ring-cyan-800"
+          >
+            Add friend
+          </Link>
+        </div>
+      </div>
+    </Card>
+  );
+};
+
+export default RelatedProfileCard;
