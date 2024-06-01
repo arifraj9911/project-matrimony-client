aac3be1ddf60802dcd4e2375fdcdec6241dbcc82
arifraj199 <karif9514@gmail.com>
2024-06-01 11:54:35 +0600

    firebase install

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAgMPO3eJm3DDrvqubDGPvOFm04M4QG4dg",
+  authDomain: "project-matrimony-9e690.firebaseapp.com",
+  projectId: "project-matrimony-9e690",
+  storageBucket: "project-matrimony-9e690.appspot.com",
+  messagingSenderId: "575444597334",
+  appId: "1:575444597334:web:4c9909c2dd11abd9b619f7"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
