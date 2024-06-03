59adc7020e62a4b3391a564ce22da33529266a95
arifraj199 <karif9514@gmail.com>
2024-06-03 23:26:01 +0600

    stripe payment service functionality implement

--- src/Pages/CheckOut/Checkout.jsx
+++ src/Pages/CheckOut/Checkout.jsx
@@ -1,24 +1,34 @@
-import { useContext } from "react";
-import {  useParams } from "react-router-dom";
+import { useContext, useState } from "react";
+import { useParams } from "react-router-dom";
 import { AuthContext } from "../../provider/AuthProvider";
 import { Button, Label, TextInput } from "flowbite-react";
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
+import PaymentCheckout from "./PaymentCheckout";
+
+const stripePromise = loadStripe(
+  "pk_test_51PLRJ5IIzM9oJ9jxP2Jgraq9LtM8Ok8eCWW78a2ymbYMgs0Uh3mr7hQ6bql7zQcknZuAr9lTuavgdJPWPIWxHVMH00JrDRoZES"
+);
 
 const Checkout = () => {
   const { user } = useContext(AuthContext);
   const { id } = useParams();
+
+  const [paymentSuccess, setPaymentSuccess] = useState(false);
+
   return (
-    <div className="w-1/4 mx-auto my-20">
+    <div className="w-1/3 mx-auto my-20">
       <h2 className="text-2xl text-center">Request Contact Information</h2>
       <hr className="my-6" />
-      <form className="flex  flex-col gap-4">
+      <div className="flex flex-col gap-4">
         <div className="flex gap-4">
-          <div>
+          <div className="w-full">
             <div className="mb-2 block">
               <Label value="Biodata ID" />
             </div>
             <TextInput value={id} readOnly type="text" required shadow />
           </div>
-          <div>
+          <div className="w-full">
             <div className="mb-2 block">
               <Label value="Amount Charged (USD)" />
             </div>
@@ -39,15 +49,18 @@ const Checkout = () => {
           />
         </div>
 
-        <div>
-          <div className="mb-2 block">
-            <Label value="Stripe Card Number" />
-          </div>
-          <TextInput type="number" required shadow />
+        <div className="my-4">
+          <Elements stripe={stripePromise}>
+            <PaymentCheckout
+              setPaymentSuccess={setPaymentSuccess}
+            ></PaymentCheckout>
+          </Elements>
         </div>
 
-        <Button type="submit">Submit</Button>
-      </form>
+        <Button disabled={paymentSuccess === false} type="submit">
+          Submit
+        </Button>
+      </div>
     </div>
   );
 };
---
+++ src/Pages/CheckOut/PaymentCheckout.jsx
@@ -0,0 +1,111 @@
+import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
+import axios from "axios";
+import { useContext, useEffect, useState } from "react";
+import { AuthContext } from "../../provider/AuthProvider";
+
+const PaymentCheckout = ({setPaymentSuccess}) => {
+  const [clientSecret, setClientSecret] = useState("");
+  const [error, setError] = useState("");
+  const [transactionId, setTransactionId] = useState("");
+  const { user } = useContext(AuthContext);
+  const stripe = useStripe();
+  const elements = useElements();
+
+  const price = 5;
+
+  useEffect(() => {
+    axios
+      .post(`http://localhost:5000/create-payment-intent`, { price })
+      .then((res) => {
+        console.log(res.data.clientSecret);
+        setClientSecret(res.data.clientSecret);
+      })
+      .catch((err) => console.log(err.message));
+  }, []);
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    if (!stripe || !elements) {
+      return;
+    }
+
+    const card = elements.getElement(CardElement);
+
+    if (card == null) {
+      return;
+    }
+
+    // payment method
+    const { error, paymentMethod } = await stripe.createPaymentMethod({
+      type: "card",
+      card,
+    });
+
+    if (error) {
+      console.log("payment error", error);
+      setError(error.message);
+    } else {
+      console.log("payment method", paymentMethod);
+      setError("");
+    }
+
+    // confirm payment
+    const { paymentIntent, error: confirmError } =
+      await stripe.confirmCardPayment(clientSecret, {
+        payment_method: {
+          card: card,
+          billing_details: {
+            email: user?.email || "anonymous",
+            name: user?.name || "anonymous",
+          },
+        },
+      });
+
+    if (confirmError) {
+      console.log("confirm error", confirmError);
+      setError(confirmError);
+    } else {
+      console.log("payment intent", paymentIntent);
+      if (paymentIntent.status === "succeeded") {
+        console.log("transaction Id:", paymentIntent.id);
+        setTransactionId(paymentIntent.id);
+        setPaymentSuccess(true);
+      }
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <CardElement
+        options={{
+          style: {
+            base: {
+              fontSize: "16px",
+              color: "#424770",
+              "::placeholder": {
+                color: "#aab7c4",
+              },
+            },
+            invalid: {
+              color: "#9e2146",
+            },
+          },
+        }}
+      />
+      <button
+        className="bg-cyan-700 text-white py-2 mt-5 rounded-md px-6"
+        type="submit"
+        disabled={!stripe || !clientSecret}
+      >
+        Pay 
+      </button>
+      <p className="text-red-600 mt-2">{error}</p>
+      {transactionId && (
+        <p className="text-green-600 mt-2">Transaction ID: {transactionId}</p>
+      )}
+    </form>
+  );
+};
+
+export default PaymentCheckout;
