a5511c6b6c560ccc08c108b463b04029251f03aa
arifraj199 <karif9514@gmail.com>
2024-06-04 00:46:20 +0600

    admin approval functionality and my contact request functionality implement

--- src/Pages/Dashboard/DashboardAdmin/ApprovedContactRequest/ApprovedContactRequest.jsx
+++ src/Pages/Dashboard/DashboardAdmin/ApprovedContactRequest/ApprovedContactRequest.jsx
@@ -1,11 +1,74 @@
-
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import { Button, Table } from "flowbite-react";
 
 const ApprovedContactRequest = () => {
-    return (
-        <div>
-            <h2>Approved Contact Request</h2>
-        </div>
-    );
+  const {
+    data: approvedContact = [],
+    isPending,
+    refetch,
+  } = useQuery({
+    queryKey: ["approvedContact"],
+    queryFn: async () => {
+      const res = await axios.get("http://localhost:5000/myRequestContact");
+      return res.data;
+    },
+  });
+
+  const handleApprovedRequest = (id) => {
+    axios
+      .put(`http://localhost:5000/myRequestContact/contactApproval/${id}`)
+      .then((res) => {
+        console.log(res.data);
+        refetch();
+      })
+      .catch((err) => console.log(err.message));
+  };
+
+  if (isPending) {
+    return <p>Loading...</p>;
+  }
+  return (
+    <div>
+      <h2 className="text-2xl">Approved Contact Request </h2>
+      <div className="overflow-x-auto my-8">
+        <Table hoverable>
+          <Table.Head>
+            <Table.HeadCell>Biodata ID</Table.HeadCell>
+            <Table.HeadCell>User Name</Table.HeadCell>
+            <Table.HeadCell>Contact Email</Table.HeadCell>
+            <Table.HeadCell>Status</Table.HeadCell>
+            <Table.HeadCell>
+              <span className="sr-only">Edit</span>
+            </Table.HeadCell>
+          </Table.Head>
+          <Table.Body className="divide-y">
+            {approvedContact?.map((request) => (
+              <Table.Row
+                key={request._id}
+                className="bg-white dark:border-gray-700 dark:bg-gray-800"
+              >
+                <Table.Cell>{request?.biodata_id}</Table.Cell>
+                <Table.Cell>{request?.name}</Table.Cell>
+                <Table.Cell>{request?.email}</Table.Cell>
+                <Table.Cell>
+                  {request?.status === "approved" ? (
+                    "Approved"
+                  ) : (
+                    <Button
+                      onClick={() => handleApprovedRequest(request.biodata_id)}
+                    >
+                      Request Contact
+                    </Button>
+                  )}
+                </Table.Cell>
+              </Table.Row>
+            ))}
+          </Table.Body>
+        </Table>
+      </div>
+    </div>
+  );
 };
 
 export default ApprovedContactRequest;
--- src/Pages/Dashboard/DashboardUser/MyContactRequest/MyContactRequest.jsx
+++ src/Pages/Dashboard/DashboardUser/MyContactRequest/MyContactRequest.jsx
@@ -1,11 +1,63 @@
-
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import { Table } from "flowbite-react";
 
 const MyContactRequest = () => {
-    return (
-        <div>
-            <h2>Contact request</h2>
-        </div>
-    );
+  const { data: myRequest = [], isPending } = useQuery({
+    queryKey: ["myRequest"],
+    queryFn: async () => {
+      const res = await axios.get("http://localhost:5000/myRequestContact");
+      return res.data;
+    },
+  });
+
+  if (isPending) {
+    return <p>Loading...</p>;
+  }
+
+  return (
+    <div>
+      <h2 className="text-2xl">My Contact Request </h2>
+      <div className="overflow-x-auto my-8">
+        <Table hoverable>
+          <Table.Head>
+            <Table.HeadCell>Biodata ID</Table.HeadCell>
+            <Table.HeadCell>User Name</Table.HeadCell>
+            <Table.HeadCell>Contact Email</Table.HeadCell>
+            <Table.HeadCell>Mobile Number</Table.HeadCell>
+            <Table.HeadCell>Status</Table.HeadCell>
+            <Table.HeadCell>
+              <span className="sr-only">Edit</span>
+            </Table.HeadCell>
+          </Table.Head>
+          <Table.Body className="divide-y">
+            {myRequest?.map((request) => (
+              <Table.Row
+                key={request._id}
+                className="bg-white dark:border-gray-700 dark:bg-gray-800"
+              >
+                <Table.Cell>{request?.biodata_id}</Table.Cell>
+                <Table.Cell>{request?.name}</Table.Cell>
+                <Table.Cell>
+                  {request?.status === "approved"
+                    ? request?.email
+                    : "Get after admin approval"}
+                </Table.Cell>
+                <Table.Cell>
+                  {request?.status === "approved"
+                    ? request?.phone
+                    : "Get after admin approval"}
+                </Table.Cell>
+                <Table.Cell>
+                  {request?.status === "approved" ? "Approved" : "Pending"}
+                </Table.Cell>
+              </Table.Row>
+            ))}
+          </Table.Body>
+        </Table>
+      </div>
+    </div>
+  );
 };
 
 export default MyContactRequest;
