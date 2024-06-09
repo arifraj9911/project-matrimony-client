910c8ef498d284b8c48f4a797b2187351ae9e96d
arifraj199 <karif9514@gmail.com>
2024-06-09 15:59:50 +0600

    pie chart added

--- src/Pages/Dashboard/DashboardAdmin/AdminDashboard/AdminDashboard.jsx
+++ src/Pages/Dashboard/DashboardAdmin/AdminDashboard/AdminDashboard.jsx
@@ -1,19 +1,57 @@
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
+// import React, { PureComponent } from 'react';
+import { Cell, Legend, Pie, PieChart } from "recharts";
+
+const COLORS = ["#0088FE", "#00C49F", "#FFBB28", "#FF8042", "#81A263"];
 
 const AdminDashboard = () => {
   const { data: biodataCount = {}, isPending } = useQuery({
     queryKey: ["biodataCount"],
     queryFn: async () => {
-      const res = await axios.get("http://localhost:5000/admin-stats",{
-        headers:{
-          authorization:`Bearer ${localStorage.getItem('access_token')}`
-        }
+      const res = await axios.get("http://localhost:5000/admin-stats", {
+        headers: {
+          authorization: `Bearer ${localStorage.getItem("access_token")}`,
+        },
       });
       console.log(res.data);
       return res.data;
     },
   });
+
+  const RADIAN = Math.PI / 180;
+  const renderCustomizedLabel = ({
+    cx,
+    cy,
+    midAngle,
+    innerRadius,
+    outerRadius,
+    percent,
+  }) => {
+    const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+    const x = cx + radius * Math.cos(-midAngle * RADIAN);
+    const y = cy + radius * Math.sin(-midAngle * RADIAN);
+
+    return (
+      <text
+        x={x}
+        y={y}
+        fill="white"
+        textAnchor={x > cx ? "start" : "end"}
+        dominantBaseline="central"
+      >
+        {`${(percent * 100).toFixed(0)}%`}
+      </text>
+    );
+  };
+
+  const pieChartData = [
+    { name: "totalBiodata", value: biodataCount.totalBiodata },
+    { name: "maleBiodata", value: biodataCount.maleBiodata },
+    { name: "femaleBiodata", value: biodataCount.femaleBiodata },
+    { name: "revenue", value: biodataCount.revenue },
+    { name: "premiumBiodata", value: biodataCount.premiumBiodata },
+  ];
 
   if (isPending) {
     return <p>Loading...</p>;
@@ -38,6 +76,28 @@ const AdminDashboard = () => {
         <div className="border p-4 shadow-sm">
           <p>Total Revenue: ${biodataCount?.revenue}</p>
         </div>
+      </div>
+      <div className="">
+        <PieChart className="mx-auto" width={600} height={400}>
+          <Pie
+            data={pieChartData}
+            cx="50%"
+            cy="50%"
+            labelLine={false}
+            label={renderCustomizedLabel}
+            outerRadius={80}
+            fill="#8884d8"
+            dataKey="value"
+          >
+            {pieChartData?.map((entry, index) => (
+              <Cell
+                key={`cell-${index}`}
+                fill={COLORS[index % COLORS.length]}
+              />
+            ))}
+          </Pie>
+          <Legend></Legend>
+        </PieChart>
       </div>
     </div>
   );
